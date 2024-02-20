f9df7818f1bcbe6249cbc2d207732fdd72a8d9e9
Nailul Ngafwa <fakea4988@gmail.com>
2024-02-20 08:54:23 +0700

    feat datatable

--- frontend/src/pages/users/column.tsx
+++ frontend/src/pages/users/column.tsx
@@ -1,13 +1,40 @@
+import { Button } from "@/components/ui/button";
 import { User } from "@/types";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<User>[] = [
   {
+    accessorKey: "index",
+    header: "No.",
+    cell: (info) => info.row.index + 1,
+  },
+  {
     accessorKey: "id",
-    header: "ID",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          User ID
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "username",
-    header: "Username",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Username
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
